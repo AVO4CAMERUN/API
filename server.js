32eea4796c2dc6db2407340f42fda07da69db12c
s5779870b <s5779870b@studenti.itisavogadro.it>
2022-01-17 20:35:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// https://www.freecodecamp.org/news/rest-api-best-practices-rest-endpoint-design-examples/
+
 // Standard module import
 const express = require('express');
 const path = require('path');
@@ -11,12 +13,14 @@ const v1 = require('./api/v1/v1');
 // Root web-app -> get interfaces rules 
 app.get('/rules/:v', (req, res) => {
     
-    // controllo che sia intero o trovare
-    // Gestire e scrivere le regole per versione delle api
-    if(typeof req.params.v === 'number'){}    
-    console.log(req.params.v);
-    //if(req.params.v)
-    res.send("Role");   //della versione corente
+    if(
+        Number.isInteger(parseInt(req.params.v)) &&
+        req.params.v > 0 && req.params.v < 2
+    ){
+        res.sendFile(path.join(__dirname, './assets/rules/ruleV' + req.params.v + '.html'));
+    } else {
+        res.sendStatus(404); 
+    }
 });
 
 // Rest-api interface v1 (Versioni non necessiarissime anche se potrebbero essere fornite alcune per inntegrazione su altri siti)
