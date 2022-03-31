b2948353b1968acecbdb05c9ae06efb816d4aee6
s5779870b <s5779870b@studenti.itisavogadro.it>
2022-03-31 23:07:16 +0200

    Import data

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Standard module import
 const express = require('express');
 const cors = require('cors')
-const path = require('path');
+
 require('dotenv').config();
 
 // Create requets rooter
@@ -12,13 +12,6 @@ app.use(cors())
 
 // Personal module import
 const v1 = require('./api/v1/v1');
-
-// Root web-app -> get interfaces rules 
-app.get('/rules/:v', (req, res) => {
-    if(+req.params.v !== 1) return res.sendStatus(404); 
-    res.sendFile(path.join(__dirname, './assets/rules/ruleV' + req.params.v + '.html'));
-});
-// express.static('')
 
 // Rest-api interface v1 (Versioni non necessiarissime anche se potrebbero essere fornite alcune per inntegrazione su altri siti)
 app.use('/api/v1', v1);
